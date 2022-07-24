027540006a3157b93c97c8facb9eb8f9f97933f7
mrparkersson <amoparkerprince78@gmail.com>
2022-07-24 14:35:01 +0000

    Add scripts to run both frontend and backend in package.json

---
+++ server/src/routes/planets/planets.router.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { getAllPlanets } = require('./planets.controller');
+
+const plantesRouter = express.Router();
+
+plantesRouter.get('/planets', getAllPlanets);
+
+module.exports = plantesRouter;
---
+++ server/src/server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const app = require('./app');
+const { loadPlanetsData } = require('./models/planets.model');
+
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 8000;
+
+async function startServer() {
+  await loadPlanetsData();
+
+  server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+startServer();
